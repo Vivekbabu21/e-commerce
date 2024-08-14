5c7235a406d193b0c4042f658c9415d8b34f2e4e
Vivekbabu21 <vivekkodurikvb@gmail.com>
2024-08-14 14:02:20 +0530

    made changes

--- index.js
+++ index.js
@@ -46,8 +46,11 @@ app.use(cookieParser());
 
 
 
-const mongoURI = process.env.MONGODB_URI;
-mongoose.connect(mongoURI)
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/E-commerce';
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
